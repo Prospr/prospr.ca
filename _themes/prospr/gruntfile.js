71b0c9fac0f1b66109df903199bd3970191f0d5d
Kenzie Rudolph-Wilson <kenzie@prospr.ca>
2014-01-11 14:52:35 -0500

    deleted some template content & minor things

--- _themes/prospr/gruntfile.js
+++ _themes/prospr/gruntfile.js
@@ -1,6 +1,7 @@
 module.exports = function(grunt){
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-compass')
 
 	grunt.initConfig({
 		uglify: {
@@ -10,12 +11,26 @@ module.exports = function(grunt){
 				}//files
 			}//myTarget
 		},//uglify
+		compass : {
+			dev: {
+				options: {
+					config: 'config.rb'
+				}//options
+			}//dev
+		},//compass
 		watch: {
 			options : { livereload: true},
 			scripts: {
 				files: ['js/components/*.js'],
 				tasks: ['uglify']
-			}//scripts
+			},//scripts
+			html: {
+				files: ['layouts/*.html', 'partials/*.html', 'templates/*.html']
+			},//html
+			sass: {
+				files: ['sass/*.scss'],
+				tasks: ['compass:dev']
+			}//sass
 		}//watch
 	})//initConfig
 	grunt.registerTask('default', 'watch');
