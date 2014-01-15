6c4fcaadf0c52b9bf009098327fbdb7862031ee0
Kenzie Rudolph-Wilson <kenzie@prospr.ca>
2014-01-15 15:23:32 -0500

    made frontpage text rotate

--- _themes/prospr/js/components/main.js
+++ _themes/prospr/js/components/main.js
@@ -2,4 +2,24 @@ $(function() {
     var BV = new $.BigVideo();
     BV.init();
     BV.show('/assets/vid/toronto.mp4',{ambient:true});
+
+    var workType = ['Design', 'Develop', 'Consult'];
+    var services = ['Small\xA0Business', 'Search\xA0Optimization', 'User\xA0Experience', 'Brand\xA0Message', 'Content\xA0Strategy'];
+
+    function rotateText (array, element){
+      var index = Math.floor(Math.random() * array.length);
+      while (index == array.indexOf(element.text())){
+        index = Math.floor(Math.random() * array.length);
+      }
+      console.log ('Random Number: ', index);
+      console.log (element);
+      $(element).fadeOut(500, function(){element.text(array[index])});
+      $(element).fadeIn();
+    }
+
+    function initTextRotate() {
+        workRotateInterval = setInterval(function(){rotateText(workType, $('.rotate-type'));}, (Math.random() * 5000) + 3000);
+        serviceRotateInterval = setInterval(function(){rotateText(services, $('.rotate-service'));}, (Math.random() * 5000) + 3000);
+    };
+    initTextRotate();
 });
