c39e8ee0f870439e1bb5e4d5c2e72d91c15e4b5e
Kenzie Rudolph-Wilson <kenzie@prospr.ca>
2014-01-30 21:12:38 -0500

    about us skeleton done & some colours changed

--- _themes/prospr/js/components/main.js
+++ _themes/prospr/js/components/main.js
@@ -1,33 +1,34 @@
 $(function() {
-    if ($('body').hasClass('home')){
-      var BV = new $.BigVideo();
-      BV.init();
-      BV.show('/assets/vid/toronto.mp4',{ambient:true});
+  if ($('body').hasClass('home')){
+    var BV = new $.BigVideo();
+    BV.init();
+    BV.show('/assets/vid/toronto.mp4',{ambient:true});
+  }
+  var workType = ['Design', 'Develop', 'Consult'];
+  var services = ['Small\xA0Business', 'Search\xA0Optimization', 'User\xA0Experience', 'Brand\xA0Message', 'Content\xA0Strategy'];
+
+  function rotateText (array, element){
+    var index = Math.floor(Math.random() * array.length);
+    while (index == array.indexOf(element.text())){
+      index = Math.floor(Math.random() * array.length);
     }
-    var workType = ['Design', 'Develop', 'Consult'];
-    var services = ['Small\xA0Business', 'Search\xA0Optimization', 'User\xA0Experience', 'Brand\xA0Message', 'Content\xA0Strategy'];
+    $(element).fadeOut(500, function(){element.text(array[index])});
+    $(element).fadeIn();
+  }
 
-    function rotateText (array, element){
-      var index = Math.floor(Math.random() * array.length);
-      while (index == array.indexOf(element.text())){
-        index = Math.floor(Math.random() * array.length);
-      }
-      $(element).fadeOut(500, function(){element.text(array[index])});
-      $(element).fadeIn();
-    }
+  function initTextRotate() {
+    workRotateInterval = setInterval(function(){rotateText(workType, $('.rotate-type'));}, (Math.random() * 5000) + 3000);
+    serviceRotateInterval = setInterval(function(){rotateText(services, $('.rotate-service'));}, (Math.random() * 5000) + 3000);
+  };
+  initTextRotate();
 
-    function initTextRotate() {
-        workRotateInterval = setInterval(function(){rotateText(workType, $('.rotate-type'));}, (Math.random() * 5000) + 3000);
-        serviceRotateInterval = setInterval(function(){rotateText(services, $('.rotate-service'));}, (Math.random() * 5000) + 3000);
-    };
-    initTextRotate();
-
-     // Init Skrollr
-    var s = skrollr.init({
-        render: function(data) {
-            //Debugging - Log the current scroll position.
-            //console.log(data.curTop);
-        },
-        forceHeight:false
-    });
+   // Init Skrollr
+  var s = skrollr.init({
+    render: function(data) {
+      //Debugging - Log the current scroll position.
+      //console.log(data.curTop);
+    },
+    smoothScrolling: true,
+    forceHeight:false
+  });
 });
